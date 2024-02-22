e09cb49e2731b2e20ecde2b16fd3f9e34e9bce8c
h114mx001 <21anh.nt@vinuni.edu.vn>
2024-02-22 09:04:32 +0700

    Update readme & skeleton code

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # gcc2024_project
+
+Assume you have Docker & Docker Compose installed. (Docker Desktop is enough to have these two)
+
+## Build 
+
+```bash 
+git clone https://github.com/UltimateHG/gcc2024_project/ && cd gcc2024_project
+cd phone_dumps_analysis 
+pip install -r requirements.txt
+docker compose up -d --build
+```
+
+## Import the transformation in Maltego
+
+![](images/specification.png)
--- phone_dumps_analysis/csv_parser.py
+++ phone_dumps_analysis/csv_parser.py
@@ -12,8 +12,21 @@ MONGODB_DATABASE = os.getenv("MONGODB_DATABASE")
 MONGODB_PORT = 27017
 
 DATABASE_CONNECTION_URI=f"mongodb://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_HOSTNAME}:{MONGODB_PORT}/{MONGODB_DATABASE}"
-# print(DATABASE_CONNECTION_URI)
+print(DATABASE_CONNECTION_URI)
 # Database 
 client = MongoClient(DATABASE_CONNECTION_URI)
 db = client[MONGODB_DATABASE]
 call_collection = db['calls']
+
+print(call_collection)
+import json
+data_json = json.load(open("test.json", "r"))
+# print(data_json)
+
+for data in data_json:
+    print(data)
+    number = data
+    sent = data_json[data]["sent"]
+    received = data_json[data]["received"]
+    print(sent, received)
+    call_collection.insert_one({"number": number, "sent": sent, "received": received})
