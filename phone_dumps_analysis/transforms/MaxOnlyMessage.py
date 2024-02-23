b805b4911b699f5250496e65dd53c13b296e13a3
h114mx001 <21anh.nt@vinuni.edu.vn>
2024-02-23 13:17:46 +0700

    Done

--- phone_dumps_analysis/transforms/MaxOnlyMessage.py
+++ phone_dumps_analysis/transforms/MaxOnlyMessage.py
@@ -4,14 +4,13 @@ from maltego_trx.transform import DiscoverableTransform
 from maltego_trx.overlays import OverlayPosition, OverlayType
 from transforms import call_collection
 
-class MaxOnly(DiscoverableTransform):
+class MaxOnlyMessage(DiscoverableTransform):
     """
     Generate a network summary for a phone number
     """
-
+ 
     @classmethod
     def create_entities(cls, request, response):
-        request.Slider = 10
         phone = request.Value 
         phone = phone.replace(" ", "")
         # try: 
@@ -24,9 +23,17 @@ class MaxOnly(DiscoverableTransform):
                 time_sent = summary["sent"][sent]
                 entity = response.addEntity(PhoneNumber, sent)
                 entity.setLinkLabel(time_sent)
-                # entity.setLinkThickness(int(time_sent))
-                
-                
+                entity.setLinkColor("#e64553")
+                entity.setLinkThickness(int(time_sent * 0.5))
+
+            for receive in summary["received"]:
+                time_receive = summary["received"][receive]
+                entity = response.addEntity(PhoneNumber, receive)
+                entity.setLinkLabel(time_receive)
+                entity.setLinkColor("#40a02b") 
+                entity.setLinkThickness(int(time_receive * 0.5))
+                entity.reverseLink()  
+
     @staticmethod
     def get_network_summary(phone):
         # Database setup. I don't why it does not work in __init__.py :)
--- phone_dumps_analysis/transforms/MinOnlyPhone.py
+++ phone_dumps_analysis/transforms/MinOnlyPhone.py
@@ -1,15 +1,17 @@
 from maltego_trx.entities import PhoneNumber
-from maltego_trx.maltego import UIM_PARTIAL
+# from maltego_trx.maltego import UIM_PARTIAL
 from maltego_trx.transform import DiscoverableTransform
-from maltego_trx.overlays import OverlayPosition, OverlayType
+# from maltego_trx.overlays import OverlayPosition, OverlayType
 from transforms import call_collection
 
-class NetworkSummary(DiscoverableTransform):
+class MinOnlyPhone(DiscoverableTransform):
     """
     Generate a network summary for a phone number
     """
+ 
     @classmethod
     def create_entities(cls, request, response):
+        request.Slider = 10
         phone = request.Value 
         phone = phone.replace(" ", "")
         # try: 
@@ -17,34 +19,28 @@ class NetworkSummary(DiscoverableTransform):
         if network_summary:
             # represent "sent" and "received" as edges
             summary = network_summary[0]
-
             for sent in summary["sent"]:
                 # print(sent)
                 time_sent = summary["sent"][sent]
                 entity = response.addEntity(PhoneNumber, sent)
                 entity.setLinkLabel(time_sent)
-                # entity.setLinkColor("#e64553")
-                # entity.setLinkThickness(int(time_sent * 0.5))
-            
-                
+                # entity.setLinkThickness(int(time_sent))
+                           
     @staticmethod
     def get_network_summary(phone):
         # Database setup. I don't why it does not work in __init__.py :)
         """
         Get a network summary for a phone number
         """
+
         query = {"number": phone}
         # print(query)
         summary = list(call_collection.find(query, {"_id": 0}))
+        # get max of the sent and received
+        max_sent = min(summary[0]["sent"].values())
+        max_received = min(summary[0]["received"].values())
+        summary[0]["sent"] = {k: v for k, v in summary[0]["sent"].items() if v == max_sent}
+        summary[0]["received"] = {k: v for k, v in summary[0]["received"].items() if v == max_received}
         # print(summary)
         return summary
 
-# test in your terminal
-if __name__ == "__main__":
-    summary = NetworkSummary.get_network_summary("0363661308")
-    summary = summary[0]
-    for received in summary["received"]:
-        print(received, summary["received"][received])
-        # received_entity = response.addEntity(PhoneNumber, received)
-        # add a link to the original phone number
-    print(summary)
